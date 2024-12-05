4fd48464ad9cd89c15da28664636e2e673728c83
Saidamin Sajid <saidamin.sajid95@gmail.com>
2024-12-05 09:42:00 +0430

    Update logout.php

--- logout.php
+++ logout.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>دانشگاه بدخشان دانشکده کمپیوترساینس</title>
+<title>Computer Science of Faculty</title>
 </head>
 
 <body>
